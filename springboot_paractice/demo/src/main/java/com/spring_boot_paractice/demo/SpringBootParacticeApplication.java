18fd6a6d215277210dff53207a3d76bb8400d303
Md Tanvir Alam <tanviralam3524@gmail.com>
2026-03-27 03:35:42 +0530

    Update SpringBootParacticeApplication.java

--- springboot_paractice/demo/src/main/java/com/spring_boot_paractice/demo/SpringBootParacticeApplication.java
+++ springboot_paractice/demo/src/main/java/com/spring_boot_paractice/demo/SpringBootParacticeApplication.java
@@ -3,12 +3,7 @@ package com.spring_boot_paractice.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-    exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-    }
-)
+@SpringBootApplication
 public class SpringBootParacticeApplication {
 
     public static void main(String[] args) {
